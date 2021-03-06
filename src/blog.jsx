3ccea9d58d0b212b47c3eb21963767707b9cce70
shuto-shinoda <shuto.88.tb@gmail.com>
2021-03-06 23:55:32 +0900

    コンポーネント間でデータの受け渡しと再利用-1-

---
+++ src/blog.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Blog extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <Article />
+      </div>
+    )
+  }
+}
+
+export default Blog
